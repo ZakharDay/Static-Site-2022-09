2c802fb96ddb77edef21476b9480c9a523a9a2a1
ZakharDay <zakhar.day@gmail.com>
2023-03-14 14:43:11 +0300

    Filters added

--- src/index.js
+++ src/index.js
@@ -1,67 +1,21 @@
 import './index.css'
 import Cookies from 'js-cookie'
 
-const selectOptions = [
-  'announcements',
-  'be your own bank',
-  'BIP39',
-  'BIP39 pdf',
-  'BIP39 word list',
-  'bitcoin',
-  'Electrum',
-  'exchange',
-  'funds safety',
-  'guides',
-  'hardware wallet',
-  'hardware wallets',
-  'Ledger',
-  'Ledger Nano',
-  'Ledger Nano S Plus',
-  'metal crypto wallet',
-  'mnemonic phrase',
-  'private key',
-  'resources',
-  'security',
-  'seed phrase',
-  'seed phrase backup',
-  'seed phrase storage',
-  'tips',
-  'Trezor One',
-  'updates',
-  'wallet',
-  'wallets'
-]
+const selectOptions = ['announcements']
 
-const multiSelectOptions = [
-  { text: 'announcements', active: false },
-  { text: 'be your own bank', active: false },
-  { text: 'BIP39', active: false },
-  { text: 'BIP39 pdf', active: false },
-  { text: 'BIP39 word list', active: false },
-  { text: 'bitcoin', active: false },
-  { text: 'Electrum', active: false },
-  { text: 'exchange', active: false },
-  { text: 'funds safety', active: false },
-  { text: 'guides', active: false },
-  { text: 'hardware wallet', active: false },
-  { text: 'hardware wallets', active: false },
-  { text: 'Ledger', active: false },
-  { text: 'Ledger Nano', active: false },
-  { text: 'Ledger Nano S Plus', active: false },
-  { text: 'metal crypto wallet', active: false },
-  { text: 'mnemonic phrase', active: false },
-  { text: 'private key', active: false },
-  { text: 'resources', active: false },
-  { text: 'security', active: false },
-  { text: 'seed phrase', active: false },
-  { text: 'seed phrase backup', active: false },
-  { text: 'seed phrase storage', active: false },
-  { text: 'tips', active: false },
-  { text: 'Trezor One', active: false },
-  { text: 'updates', active: false },
-  { text: 'wallet', active: false },
-  { text: 'wallets', active: false }
-]
+const multiSelectOptions = []
+
+const content = []
+
+function transformToLowercase(array) {
+  const transformedArray = []
+
+  array.forEach((item) => {
+    transformedArray.push(item.toLowerCase())
+  })
+
+  return transformedArray
+}
 
 function updateSelectData(option) {
   multiSelectOptions.forEach((o) => {
@@ -69,6 +23,137 @@ function updateSelectData(option) {
       o.active = !option.active
     }
   })
+}
+
+function updateContent() {
+  const contentItems = document.getElementsByClassName('O_ContentItem')
+  const selectedTags = []
+
+  multiSelectOptions.forEach((item) => {
+    if (item.active) {
+      selectedTags.push(item.text)
+    }
+  })
+
+  console.log('selectedTags', selectedTags)
+
+  for (let i = 0; i < contentItems.length; i++) {
+    const contentItem = contentItems[i]
+    const contentItemTags = transformToLowercase(
+      contentItem.dataset.tags.split(',')
+    )
+
+    contentItem.classList.remove('hidden')
+
+    selectedTags.forEach((tag) => {
+      if (!contentItemTags.includes(tag)) {
+        contentItem.classList.add('hidden')
+      }
+    })
+  }
+}
+
+function createContentCard(contentItemData) {
+  const contentItem = document.createElement('div')
+  contentItem.classList.add('O_ContentItem')
+
+  const contentItemCover = document.createElement('img')
+  contentItemCover.classList.add('A_ContentItemCover')
+  contentItemCover.src = contentItemData.image
+
+  const contentItemTags = document.createElement('div')
+  contentItemTags.classList.add('C_ContentItemTags')
+
+  contentItemData.tags.forEach((tag) => {
+    const contentItemTag = document.createElement('div')
+    contentItemTag.classList.add('A_ContentItemTag')
+    contentItemTag.innerText = tag
+    contentItemTags.appendChild(contentItemTag)
+  })
+
+  const contentItemTitle = document.createElement('h2')
+  contentItemTitle.classList.add('A_ContentItemTitle')
+  contentItemTitle.innerText = contentItemData.title
+
+  const contentItemDescription = document.createElement('p')
+  contentItemDescription.classList.add('A_ContentItemDescription')
+  contentItemDescription.innerText = contentItemData.description
+
+  contentItem.appendChild(contentItemCover)
+  contentItem.appendChild(contentItemTags)
+  contentItem.appendChild(contentItemTitle)
+  contentItem.appendChild(contentItemDescription)
+
+  return contentItem
+}
+
+function rerenderContent() {
+  const contentItemsContainer = document.querySelector('.S_Content')
+  const selectedTags = []
+
+  multiSelectOptions.forEach((item) => {
+    if (item.active) {
+      selectedTags.push(item.text)
+    }
+  })
+
+  console.log('selectedTags', selectedTags)
+
+  contentItemsContainer.innerHTML = ''
+
+  for (let i = 0; i < content.length; i++) {
+    const contentItem = content[i]
+
+    if (selectedTags.length > 0) {
+      selectedTags.forEach((tag) => {
+        if (transformToLowercase(contentItem.tags).includes(tag)) {
+          console.log('true')
+          contentItemsContainer.appendChild(createContentCard(contentItem))
+        } else {
+          console.log('false')
+        }
+      })
+    } else {
+      contentItemsContainer.appendChild(createContentCard(contentItem))
+    }
+  }
+}
+
+// function getContentItemDataTags() {
+//   const contentItems = document.getElementsByClassName('O_ContentItem')
+//   const tags = []
+
+//   for (let i = 0; i < contentItems.length; i++) {
+//     const contentItem = contentItems[i]
+//     const contentItemTags = contentItem.dataset.tags.split(',')
+
+//     // contentItemTags.forEach((item) => {
+//     //   tags.push(item)
+//     // })
+
+//     tags.push(...contentItemTags)
+//   }
+
+//   const uniqTags = transformToLowercase([...new Set(tags)])
+//   // const uniqTagsTransformed = []
+
+//   // uniqTags.forEach((item) => {
+//   //   uniqTagsTransformed.push(item.toLowerCase())
+//   // })
+
+//   return uniqTags.sort()
+// }
+
+function getContentItemDataTags() {
+  const tags = []
+
+  for (let i = 0; i < content.length; i++) {
+    tags.push(...content[i].tags)
+  }
+
+  const uniqTags = transformToLowercase([...new Set(tags)])
+
+  return uniqTags.sort()
 }
 
 function updateSelectOptionList() {
@@ -89,6 +174,7 @@ function updateSelectOptionList() {
       listItem.addEventListener('click', () => {
         updateSelectData(option)
         updateSelectOptionList()
+        rerenderContent()
 
         const chipElement = createChip(option)
         selectInput.appendChild(chipElement)
@@ -116,6 +202,7 @@ function createChip(option) {
   chipElementButton.addEventListener('click', () => {
     updateSelectData(option)
     updateSelectOptionList()
+    rerenderContent()
     chipElement.remove()
   })
 
@@ -125,10 +212,51 @@ function createChip(option) {
   return chipElement
 }
 
+// function initMultiSelect() {
+//   const selectElement = document.querySelector('.O_MultiSelect')
+//   const selectInput = document.querySelector('.A_MultiSelectInput')
+//   const dropdownButton = document.querySelector('.A_MultiSelectDropdownButton')
+
+//   getContentItemDataTags().forEach((item) => {
+//     multiSelectOptions.push({
+//       text: item,
+//       active: false
+//     })
+//   })
+
+//   updateSelectOptionList()
+
+//   dropdownButton.addEventListener('click', () => {
+//     selectElement.classList.toggle('focus')
+//   })
+
+//   selectInput.addEventListener('focus', () => {
+//     selectElement.classList.add('focus')
+//   })
+
+//   document.body.addEventListener('click', (e) => {
+//     console.log(multiSelectOptions)
+
+//     if (
+//       !e.target.classList.contains('A_MultiSelectInput') &&
+//       !e.target.classList.contains('A_MultiSelectDropdownButton')
+//     ) {
+//       selectElement.classList.remove('focus')
+//     }
+//   })
+// }
+
 function initMultiSelect() {
   const selectElement = document.querySelector('.O_MultiSelect')
   const selectInput = document.querySelector('.A_MultiSelectInput')
   const dropdownButton = document.querySelector('.A_MultiSelectDropdownButton')
+
+  getContentItemDataTags().forEach((item) => {
+    multiSelectOptions.push({
+      text: item,
+      active: false
+    })
+  })
 
   updateSelectOptionList()
 
@@ -231,10 +359,47 @@ function initModal() {
   }
 }
 
+function initFilters() {
+  const contentItems = document.getElementsByClassName('O_ContentItem')
+
+  for (let i = 0; i < contentItems.length; i++) {
+    const contentItem = contentItems[i]
+    const contentItemTags = contentItem.dataset.tags.split(',')
+
+    const contentItemImage = contentItem.querySelector(
+      '.A_ContentItemCover'
+    ).src
+
+    const contentItemTitle = contentItem.querySelector(
+      '.A_ContentItemTitle'
+    ).innerText
+
+    const contentItemDescription = contentItem.querySelector(
+      '.A_ContentItemDescription'
+    ).innerText
+
+    const contentItemData = {
+      tags: contentItemTags,
+      image: contentItemImage,
+      title: contentItemTitle,
+      description: contentItemDescription
+    }
+
+    content.push(contentItemData)
+  }
+
+  console.log('content', content)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   if (document.body.classList.contains('index')) {
     initModal()
     initSelect()
+
+    // getContentItemDataTags()
+    // initMultiSelect()
+
+    initFilters()
     initMultiSelect()
   }
 })
